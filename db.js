4a954218f2622ef3625766329aae64aec765ea7e
Aditya Dubey <164205700+Dubeaditya@users.noreply.github.com>
2026-03-19 19:57:43 +0530

    Update port db.js

--- db.js
+++ db.js
@@ -5,7 +5,11 @@ const connection = mysql.createConnection({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false 
+    },
+    connectTimeout: 20000
 });
 
 connection.connect((err) => {
